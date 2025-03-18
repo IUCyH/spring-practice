663e041f6d4c4e1448325f3d3e67aa6874093ab0
Jaemin <ghdckdwn8456@gmail.com>
2025-03-18 18:13:16 +0900

    [feat] 로그 파일 생성 구현

--- Project/src/main/java/com/iucyh/common/interceptor/LoggingInterceptor.java
+++ Project/src/main/java/com/iucyh/common/interceptor/LoggingInterceptor.java
@@ -5,18 +5,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class LoggingInterceptor implements HandlerInterceptor {
     public LoggingInterceptor() {}
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         if(handler instanceof HandlerMethod) {
             HandlerMethod hm = (HandlerMethod) handler;
-            System.out.println(request.getRequestURI() + " " + hm.getMethod().getName());
-        }
 
-        return true;
+            log.info("Request URL: {}", request.getRequestURI());
+            log.info("Response Status: {}", response.getStatus());
+        }
     }
 }
